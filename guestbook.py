fbc2182bf7e579fe10e1cd7ad717653b43a26f05
Aryan Naraghi <aryan.naraghi@gmail.com>
2015-06-09 19:34:21 -0700

    Adds a form to the guestbook.

--- guestbook.py
+++ guestbook.py
@@ -1,11 +1,23 @@
 import webapp2
 
+PAGE = """\
+<!doctype html>
+<html>
+  <body>
+    <form action="/" method="POST">
+      <textarea name="content" rows="5" cols="60"></textarea>
+      <input type="submit" value="Sign!" />
+    </form>
+  </body>
+</html>
+"""
+
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, world!')
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write(PAGE)
 
 
 app = webapp2.WSGIApplication([
