164684b60bcf7ca7f6fe540443e34100580f355f
Aryan Naraghi <aryan.naraghi@gmail.com>
2015-06-09 19:41:15 -0700

    Adds a very naive way of persisting data.

--- guestbook.py
+++ guestbook.py
@@ -1,3 +1,4 @@
+import cStringIO
 import webapp2
 
 PAGE = """\
@@ -8,16 +9,27 @@ PAGE = """\
       <textarea name="content" rows="5" cols="60"></textarea>
       <input type="submit" value="Sign!" />
     </form>
+    {existing_posts}
   </body>
 </html>
 """
+
+POSTS = []
 
 
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.response.write(PAGE)
+        formatted_posts = cStringIO.StringIO()
+        for post in POSTS:
+            formatted_posts.write('    <p>{0}</p>\n'.format(post))
+        self.response.write(PAGE.format(
+                existing_posts=formatted_posts.getvalue()))
+
+    def post(self):
+        POSTS.append(self.request.get('content'))
+        self.redirect('/')
 
 
 app = webapp2.WSGIApplication([
